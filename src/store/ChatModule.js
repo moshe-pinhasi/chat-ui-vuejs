b6fe81f4a3d92e71b8f2911997c54587a83b1420
moshe-pinhasi <pi.moshe@gmail.com>
2018-01-06 22:35:24 +0200

    move the socket to store

--- src/store/ChatModule.js
+++ src/store/ChatModule.js
@@ -1,30 +1,37 @@
 'use strict'
 
 const ADD_MESSGAE = 'chat/addMessage'
-export const SOCKET_SPOTIM_CHAT = 'socket_spotimChat'
+const SOCKET_SPOTIM_CHAT = 'socket_spotimChat'
+export const SEND_MESSGAE = 'chat/sendMessage'
 
-export default {
-  state: {
-    connected: false,
-    messages: []
-  },
-  getters: {
-    messages: state => state.messages
-  },
-  mutations: {
-    'SOCKET_CONNECT': (state) => {
-      state.connected = true
+export default function createChatModule (socket) {
+  return {
+    state: {
+      connected: false,
+      messages: []
     },
-    'SOCKET_DISCONNECT': (state) => {
-      state.connected = false
+    getters: {
+      messages: state => state.messages
     },
-    [ADD_MESSGAE]: (state, message) => {
-      state.messages.push(message)
-    }
-  },
-  actions: {
-    [SOCKET_SPOTIM_CHAT]: (context, message) => {
-      context.commit(ADD_MESSGAE, message)
+    mutations: {
+      'SOCKET_CONNECT': (state) => {
+        state.connected = true
+      },
+      'SOCKET_DISCONNECT': (state) => {
+        state.connected = false
+      },
+      [ADD_MESSGAE]: (state, message) => {
+        state.messages.push(message)
+      }
+    },
+    actions: {
+      [SOCKET_SPOTIM_CHAT]: (context, message) => {
+        context.commit(ADD_MESSGAE, message)
+      },
+      [SEND_MESSGAE]: ({rootGetters}, { message }) => {
+        message = Object.assign({}, rootGetters.user, {text: message})
+        socket.emit('spotim/chat', message)
+      }
     }
   }
 }
